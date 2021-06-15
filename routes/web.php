d49b7e856ea0f4fb94da222bb4bf095694b7282b
renatocastroct <81470516+renatocastroct@users.noreply.github.com>
2021-06-15 22:27:06 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('home')
     ->with('csrf_token', csrf_token());
@@ -25,27 +13,23 @@ Route::get('home', function () {
 });
 Route::get('production', function () {
     return view('production')
-    ->with('csrf_token', csrf_token());;
+    ->with('csrf_token', csrf_token());
 });
 Route::get('signup', function () {
     return view('signup')
-    ->with('csrf_token', csrf_token());;
+    ->with('csrf_token', csrf_token());
 });
 Route::get('statistics', function () {
     return view('statistics')
-    ->with('csrf_token', csrf_token());;
+    ->with('csrf_token', csrf_token());
 });
 
 Route::post('user', 'App\Http\Controllers\LoginHomeController@homeUser');
 
 Route::get('department', 'App\Http\Controllers\LoginHomeController@homeDepartment');
-Route::get('home/department', 'App\Http\Controllers\LoginHomeController@homeDepartment');
-Route::get('production/department', 'App\Http\Controllers\LoginHomeController@homeDepartment');
 
-Route::get('production/notlog', 'App\Http\Controllers\LoginHomeController@homeNotLog');
 Route::get('notlog', 'App\Http\Controllers\LoginHomeController@homeNotLog');
 
-Route::get('production/managelots/{initCall}/{lot?}/{product?}/{flag?}/', 'App\Http\Controllers\ManageLotsController@manageLots');
 Route::get('managelots/{initCall}/{lot?}/{product?}/{flag?}/', 'App\Http\Controllers\ManageLotsController@manageLots');
 
 
@@ -66,4 +50,3 @@ Route::post('signupvalidation', 'App\Http\Controllers\SignupController@signupVal
 Route::get('statistics/comparison', 'App\Http\Controllers\StatisticsController@quotes');
 
 Route::get('logout/{send}', 'App\Http\Controllers\LoginHomeController@logout');
-
